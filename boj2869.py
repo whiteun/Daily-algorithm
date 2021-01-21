d32ab77d2603148aa6b73504d7a59d78a53bf5fb
yeeun <maple1480@naver.com>
2021-01-21 12:33:46 +0900

    백준 2869번 달팽이는 올라가고 싶다

---
+++ boj2869.py
@@ -0,0 +1,13 @@
+'''
+백준 2869번
+달팽이는 올라가고 싶다
+BRONZ 2
+'''
+
+a, b, h = map(int, input().split())  
+
+def snail(h, d, n):
+    ret = (h-d) // (d-n) +1
+    return ret if not ((h-d) % (d-n)) else ret +1
+
+print(snail(h, a, b))
